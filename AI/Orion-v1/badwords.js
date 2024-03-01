772506f0570ceda4a1a3d682208f1d97c99aa9c2
Breakout-X <153556885+Breakout-X@users.noreply.github.com>
2024-03-01 13:02:05 -0500

    Update badwords.js

--- AI/Orion-v1/badwords.js
+++ AI/Orion-v1/badwords.js
@@ -1,5 +1,5 @@
 // dont read this
 export const inappropriateWords = ['ass', 'asses', 'bitch', 'bitches', 'btch', 'cuck', 'cock', 'cunt', 'damn', 'dick', 'fuck', 'fucker', 'gay', 'lesbian', 'nigger', 'niga', 'nig', 'shit', 'shits', 'lmao', 'tf', 'mf'];
 // Add any inappropriate words here
-window.inappropiateWords = inapropiateWords;
+window.inappropriateWords = inappropriateWords;
 // this is embarrassing but i can't let people do whatever
