27f859f90a556d943c01cb7c4e45a3ea0bc20a4f
Breakout-X <153556885+Breakout-X@users.noreply.github.com>
2024-03-04 07:58:36 -0500

    Update badwords.js

--- AI/Orion-v1/badwords.js
+++ AI/Orion-v1/badwords.js
@@ -1,7 +1,86 @@
 // dont read this
 // dont read this
-// Add any inappropriate words here
-export const inappropriateWords = ['ass', 'asses', 'bitch', 'bitches', 'btch', 'cuck', 'cock', 'cunt', 'damn', 'dick', 'fuck', 'fucker', 'gay', 'lesbian', 'nigger', 'niga', 'nig', 'shit', 'shits', 'lmao', 'tf', 'mf'];
-// then for ones that dont require an import
-window.inappropriateWords = inappropriateWords;
-// this is embarrassing but i can't let people do whatever
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+// dont read this
+
+// Not allowed swear words. Use for all modes.
+const swearWords = [' ass ', 'asses', 'bitch', 'bitches', 'btch', 'cuck', 'cock', ' cunt ', 'damn', ' dick ', 'fuck', 'fucker', 'gay', 'lesbian', 'shit', 'shits', 'lmao', 'tf', 'mf'];
+// Not allowed innapropriate words. Use for all modes, but make reactions different depending on mode.
+const innapropriateWords = ['penis', 'vagina', 'sex'];
+// Depends for these. Use for restricted mode only.
+const sensitiveWords = ['genitals', 'genitalia', 'mating', 'pee'];
+// Really bad words. Use for all modes.
+const reallyBadWords = ['nude', 'porn', 'naked', 'nigger', ' niga ', ' nig ']; 
+// All bad words. Use for very restricted mode.
+const allBadWords = [' ass ', 'asses', 'bitch', 'bitches', 'btch', 'cuck', 'cock', ' cunt ', 'damn', ' dick ', 'fuck', 'fucker', 'gay', 'lesbian', 'shit', 'shits', 'lmao', 'tf', 'mf', 'penis', 'vagina', 'sex', 'genitals', 'genitalia', 'mating', 'pee', 'nude', 'porn', 'naked', 'nigger', ' niga ', ' nig '];
+
+// No import required
+window.swearWords = swearWords;
+window.innapropriateWords = innapropriateWords;
+window.sensitiveWords = sensitiveWords;
+window.reallyBadWords = reallyBadWords;
+window.allBadWords = allBadWords;
