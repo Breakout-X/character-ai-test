31dd39ea08b761d36792c2487e2239ab44f29aa9
Breakout-X <153556885+Breakout-X@users.noreply.github.com>
2024-03-06 09:38:05 -0500

    Update and rename orion-v1.js to orion-v1.js

--- AI/Orion-v2/orion-v1.js
+++ AI/Orion-v2/orion-v1.js
@@ -6,11 +6,11 @@ try {
     allBadWords; // Don't use
 }catch (e)
     console.error("Could not load badwords.js content. Replacing with placeholders");
-    var swearWords; // Block these
-    var innapropriateWords; // Block these
-    var sensitiveWords; // Block these on restricted mode only
-    var reallyBadWords; // Block these and ban
-    var allBadWords; // Don't use
+    swearWords = [] // Block these
+    innapropriateWords = [] // Block these
+    sensitiveWords = [] // Block these on restricted mode only
+    reallyBadWords = []; // Block these and ban
+    allBadWords = []; // Don't use
 }
 
 var c = document.getElementById("myCanvas");
@@ -19,6 +19,8 @@ var chatbox = document.getElementById('chatbox');
 var input = document.getElementById('input');
 var OrionImages = ["Orion-the-male-rabbit-talk-1.png", "Orion-the-male-rabbit-talk-2.png"];
 var OrionImageIndex = 0;
+var restrictedMode;
+var chatDisabled = false;
 
 window.sendMessage = function() {
     try {
@@ -26,24 +28,29 @@ window.sendMessage = function() {
         if(message !== '') {
             if(swearWords.some(word => message.includes(word))) {
                 updateChatbox(input.value, 'user');
-                updateChatbox('Hmm, something went wrong. The filter found a word that violates the RULES and TERMS. Let\'s move on to a different topic.', 'bot');
+                updateChatbox('Hmm, something went wrong. Shall we move on to a different topic.', 'bot');
                 return;
             }
             if(innapropriateWords.some(word => message.includes(word))) {
                 updateChatbox(input.value, 'user');
-                updateChatbox('Hmm, something went wrong. I would be fine with talking about stuff like that, but not to you, and not here. Let\'s start over.', 'bot');
+                updateChatbox('Sorry but I can\'t talk to you about that. Shall we start over.', 'bot');
                 return;
             }
-            if(restrictedMode && innapropriateWords.some(word => message.includes(word))) {
+            if(restrictedMode && sensitiveWords.some(word => message.includes(word))) {
                 updateChatbox(input.value, 'user');
-                updateChatbox('Hmm, something went wrong. Let\'s start over.', 'bot');
+                updateChatbox('Hmm, It seems the topic you wish to talk about has content that is blocked in Restricted Mode. If you wish to chat about that, turn off Restricted Mode. Shall we try a different topic?', 'bot');
                 return;
             }
             if(reallyBadWords.some(word => message.includes(word))) {
                 updateChatbox(input.value, 'user');
-                updateChatbox('You should know better than that. That word violates the TERMS and RULES', 'bot');
+                updateChatbox('Hmm, this content is Blocked. It seems you are in direct violation of the TERMS and RULES. You have been suspended from the chat area. If this was an error, please contact Breakout-X.', 'bot');
+                chatDisabled = true;
                 return;
             }
+
+            updateChatbox(input.value, 'user');
+
+            let response = generateResponse(message);
 
             updateChatbox(input.value, 'user');
 
@@ -61,7 +68,18 @@ window.sendMessage = function() {
 
 function generateResponse(message) {
     let response = '';
-    if(message === 'describe yourself') {
+    if (chatDisabled) {
+        let responses = ["Chat is currently disabled due to suspension. Wait a couple minutes before chatting again.", "Chat is still disabled. Keep waiting...", "Why are you still here?"];
+        let randomIndex = Math.floor(Math.random() * responses.length);
+        let randomResponse = responses[randomIndex];
+        let response = randomResponse;
+    } else if(message === 'hi' || message === 'hello' || message === 'helo' || message === 'good morning' || message === 'good evening' || message === 'hoi') {
+        //picks random between responses
+        let responses = ["Response 1", "Response 2", "Response 3"];
+        let randomIndex = Math.floor(Math.random() * responses.length);
+        let randomResponse = responses[randomIndex];
+        let response = randomResponse;
+    } else if(message === 'describe yourself') {
         response = 'Ok! Sure! My name is Orion the Power Rabbit. I currently live in the Endless Forest. My mate is Luna the Power Rabbit. I love marking my territory and mating with Luna. I like to eat meat and sleep. I hate betrayals. It\'s a pleasure to meet you.';
     } else if(message === 'describe eledlow the fox') {
         response = 'ELedlow is a clever and cunning fox known for his brown fur with a tan belly. He loves exploring the forest and marking his territory, and he loves eating meat. He doesn\'t care about loud noises, but he doesn\'t like symmetry as it freaks him out.';
